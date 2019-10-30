db37c263f9875a937cfcc836f852fdad3d03256f
Yung-Chen Chou <yungchen@asia.edu.tw>
2019-10-30 15:52:36 +0800

    1

---
+++ src/M2Q5.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class M2Q5 {
+
+    public static void main(String[] args) {
+        Scanner scn=new Scanner(System.in);
+        int n=scn.nextInt();
+        String str="";
+        while (n>=1){
+            str=Integer.toString(n%2)+str;
+            n=n/2;
+        }
+
+
+        System.out.println(str);
+    }
+}
--- src/q4.java
+++ src/q4.java
@@ -1,21 +1,25 @@
 import java.util.Scanner;
 
-public class Main {
+public class q4 {
 
     public static void main(String[] args) {
         Scanner scn=new Scanner(System.in);
         int n=scn.nextInt();
+        int a,b,c;
         while(n!=-999){
             if(n>0){
-
+                a++;
             }
             else if(n==0){
+                b++;
 
             }
             else{
+                c++;
 
             }
             n=scn.nextInt();
         }
+        System.out.println();
     }
 }
